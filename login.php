c2fd1a11280bea80005f1bbf7332b6e3b0bed241
hersht <trulee.hersh@gmail.com>
2018-10-30 13:03:26 -0400

    updated titles

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php 
   include('includes/init.inc.php'); // include the DOCTYPE and opening tags
 ?>
-<title>RoomRez Home</title>
+<title>RPI Events</title>
 
 <?php 
   include('includes/head.inc.php'); // include global css, javascript, end the head and open the body
