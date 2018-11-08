0848bbd380ea8fce1c083faf48f6e1116a2bc074
zhangc13 <zhangc13@rpi.edu>
2018-11-08 13:24:22 -0500

    pushing updated database. user table was modified

--- resources/sql/rpi_events.sql
+++ resources/sql/rpi_events.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 30, 2018 at 05:52 PM
+-- Generation Time: Nov 08, 2018 at 07:23 PM
 -- Server version: 10.1.26-MariaDB
 -- PHP Version: 7.1.9
 
@@ -51,6 +51,13 @@ CREATE TABLE `events` (
   `owner` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `events`
+--
+
+INSERT INTO `events` (`id`, `title`, `date`, `start`, `end`, `location`, `description`, `owner`) VALUES
+(1, 'Study group', '2018-11-07', '10:30:00', '12:00:00', 'Folsom Library ', 'Join us for a bio study group.', '1');
+
 -- --------------------------------------------------------
 
 --
@@ -58,18 +65,21 @@ CREATE TABLE `events` (
 --
 
 CREATE TABLE `users` (
-  `email` varchar(255) NOT NULL,
-  `firstname` varchar(255) NOT NULL,
-  `lastname` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `userID` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `salt` varchar(100) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `firstName` varchar(40) NOT NULL,
+  `lastName` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`email`, `firstname`, `lastname`, `password`) VALUES
-('zhangc13@rpi.edu', 'Carolyn', 'Zhang', 'password');
+INSERT INTO `users` (`userID`, `username`, `password`, `salt`, `email`, `firstName`, `lastName`) VALUES
+(1, '', '33b48475d713da7f6d07219ffe5e6e529ba7745cf37983b0d3f06d628c86593a', 'af95a6849f70cab4ced98638ac94d6680bf667530da22cf6918252de2f2965de', 'jdoe@rpi.edu', 'John', 'Doe');
 
 --
 -- Indexes for dumped tables
@@ -82,6 +92,12 @@ ALTER TABLE `events`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userID`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -89,7 +105,13 @@ ALTER TABLE `events`
 -- AUTO_INCREMENT for table `events`
 --
 ALTER TABLE `events`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
